38e239e04b132d098f43360e50f4e7f8f5ab28a7
molly hickman <mollygh@vt.edu>
2019-10-08 16:40:05 -0400

    english, hindi and urdu page contents data

--- python/getdata.py
+++ python/getdata.py
@@ -1,15 +1,34 @@
 import wikipedia
 import csv
 
-wikipedia.set_lang("hi")
-
 articles_eng = {"Kashmir Conflict", "Article 370 of the Constitution of India", "Insurgency in Jammu and Kashmir"}
            
 articles_hin = {"कश्मीर_विवाद":"Kashmir_conflict_hindi", "अनुच्छेद_३७०": "Article_370_hindi", "जम्मू_और_कश्मीर_में": "Insurgency_hindi"}
 
+articles_urdu = {"مسئلہ_کشمیر": "Kashmir_conflict_urdu", "آئین_ہند_کی_دفعہ_370": "Article_370_urdu"}
+
+# Hindi
+wikipedia.set_lang("hi")
 
 for pagename, fname in articles_hin.items():
-    print('ehy')
     page = wikipedia.page(pagename)
-    with open(fname + "_contents.txt", "w") as myfile:
+    with open("../data/contents/" + fname + "_contents.txt", "w") as myfile:
+        myfile.write(page.content)
+
+
+# English
+wikipedia.set_lang("en")
+
+for pagename in articles_eng:
+    page = wikipedia.page(pagename)
+    with open("../data/contents/" + pagename + "_eng_contents.txt", "w") as myfile:
+        myfile.write(page.content)
+
+
+# Urdu
+wikipedia.set_lang("ur")
+
+for pagename, fname in articles_urdu.items():
+    page = wikipedia.page(pagename)
+    with open("../data/contents/" + fname + "_contents.txt", "w") as myfile:
         myfile.write(page.content)
