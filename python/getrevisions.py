bb7a561fa29a30dc40c42e69b2efec8a0a50786f
molly hickman <mollygh@vt.edu>
2019-10-15 13:31:49 -0400

    hindi and urdu revisions on article 370 and kashmir conflict (insurgency article not found on hi.wiki... for some reason, but OK since we decided to exclude insurgency article)

--- python/getrevisions.py
+++ python/getrevisions.py
@@ -63,3 +63,95 @@ for fname, params in EN_DICT.items():
         #print(page["revisions"])
         with open("../data/rev_" + fname + "_" + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".txt", "w+") as file:
                 file.write(str(page["revisions"]))
+
+
+#====================HINDI====================
+
+URL = "https://hi.wikipedia.org/w/api.php"
+
+KASH_HI_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "कश्मीर_विवाद", #Kashmir conflict
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying   
+}
+
+ARTICLE_HI_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "अनुच्छेद_३७०", #Article 370
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying      
+}
+
+INSURG_HI_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "जम्मू_और_कश्मीर_में",
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying      
+}
+
+HI_DICT = {"kash_hi": KASH_HI_PARAMS, "article_hi": ARTICLE_HI_PARAMS, "insurg_hi": INSURG_HI_PARAMS}
+
+for fname, params in HI_DICT.items():
+    R = S.get(url=URL, params=params)
+    DATA = R.json()
+
+    PAGES = DATA["query"]["pages"]
+
+    for page in PAGES:
+        print(page)
+        #print(page["revisions"])
+        with open("../data/rev_" + fname + "_" + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".txt", "w+") as file:
+                file.write(str(page["revisions"]))
+
+#====================URDU====================
+
+URL = "https://ur.wikipedia.org/w/api.php"
+
+KASH_UR_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "مسئلہ_کشمیر", #Kashmir conflict
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying   
+}
+
+ARTICLE_UR_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "آئین_ہند_کی_دفعہ_370", #Article 370
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying      
+}
+
+UR_DICT = {"kash_ur": KASH_UR_PARAMS, "article_ur": ARTICLE_UR_PARAMS}
+
+for fname, params in UR_DICT.items():
+    R = S.get(url=URL, params=params)
+    DATA = R.json()
+
+    PAGES = DATA["query"]["pages"]
+
+    for page in PAGES:
+        print(page)
+        #print(page["revisions"])
+        with open("../data/rev_" + fname + "_" + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".txt", "w+") as file:
+                file.write(str(page["revisions"]))
