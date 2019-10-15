bde5624d41b453e6759833978222440f274036c5
molly hickman <mollygh@vt.edu>
2019-10-15 13:07:44 -0400

    revision data for all three english articles

--- python/getrevisions.py
+++ python/getrevisions.py
@@ -17,7 +17,7 @@ S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
-PARAMS = {
+KASH_EN_PARAMS = {
     "action": "query",
     "prop": "revisions",
     "titles": "Kashmir conflict",
@@ -28,12 +28,38 @@ PARAMS = {
     "rvlimit": "500" # Cap at 500 queries which is annoying
 }
 
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
+ARTICLE_EN_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "Article 370 of the Constitution of India",
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying
+}
 
-PAGES = DATA["query"]["pages"]
+INSURG_EN_PARAMS = {
+    "action": "query",
+    "prop": "revisions",
+    "titles": "Insurgency in Jammu and Kashmir",
+    "rvprop": "timestamp|user|comment", # there's more could add like content
+    "rvslots": "main",
+    "formatversion": "2",
+    "format": "json",
+    "rvlimit": "500" # Cap at 500 queries which is annoying   
+}
 
-for page in PAGES:
-    print(page["revisions"])
-    with open("rev_kash_en_" + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".txt", "w+") as file:
-            file.write(str(page["revisions"]))
+EN_DICT = {"kash_en": KASH_EN_PARAMS, "article_en": ARTICLE_EN_PARAMS, "insurg_en": INSURG_EN_PARAMS}
+
+for fname, params in EN_DICT.items():
+    R = S.get(url=URL, params=params)
+    DATA = R.json()
+
+    PAGES = DATA["query"]["pages"]
+
+    for page in PAGES:
+        print(page)
+        #print(page["revisions"])
+        with open("../data/rev_" + fname + "_" + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".txt", "w+") as file:
+                file.write(str(page["revisions"]))
